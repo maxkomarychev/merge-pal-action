806faaf998201dfffed3eb63688de22cc5c7677f
Max Komarychev <maxkomarychev@gmail.com>
2019-11-16 22:56:31 +0200

    wip1

--- src/pushHandler.ts
+++ src/pushHandler.ts
@@ -1,7 +1,16 @@
-import { Client, Context, Config } from './types'
+import { Client, Context, Config, PushPayload } from './types'
 
-export default async function prHandler(
+export default async function pushHandler(
     client: Client,
     context: Context,
     config: Config,
-) {}
+) {
+    const payload = context.payload as PushPayload
+    const components = payload.ref.split('/')
+    const branchName = components[components.length - 1]
+    const openedPrs = client.pulls.list({
+        ...context.repo,
+        state: 'open',
+        base: branchName,
+    })
+}
